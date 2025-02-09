8b4d4308a74fefe0e6b61b063eef273a2e0ccb48
Kaique Motta <kaique.motta@hotmail.com>
2025-02-09 18:08:12 -0300

    feat: go.yml

--- routes/routes.go
+++ routes/routes.go
@@ -3,21 +3,17 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"payments/delivery" // Para importar o handler (delivery)
+	"payments/delivery"
 	"payments/usecase"
 )
 
-// RegisterPaymentRoutes registra todas as rotas de pagamentos
 func RegisterPaymentRoutes(app *fiber.App, useCase usecase.PaymentUseCase) {
-	// Cria uma instância do handler, que é responsável por interagir com a delivery
 	handler := delivery.NewPaymentHandler(useCase)
 
-	// Registra as rotas com seus respectivos métodos
 	app.Get("/payments", handler.GetAllPayments)
 	app.Get("/payments/:id", handler.GetPaymentByID)
 	app.Post("/payments", handler.CreatePayment)
 	app.Put("/payments/:id", handler.UpdatePayment)
 	app.Delete("/payments/:id", handler.DeletePayment)
 	app.Post("/payment/callback", handler.Callback)
-
 }
