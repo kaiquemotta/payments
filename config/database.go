e99d6ad5155239835e1cee30fad07aff4114e93c
Kaique Motta <kaique.motta@hotmail.com>
2025-02-11 20:08:17 -0300

    feat: segragando config database.go

--- config/database.go
+++ config/database.go
@@ -2,31 +2,36 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"time"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// MongoDB é a instância do banco de dados MongoDB acessível globalmente
 var MongoDB *mongo.Database
 
 func InitDB() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		log.Fatal("DB_PASSWORD não está definido")
+	}
+	uri := fmt.Sprintf("mongodb+srv://kaiquemotta:%s@payments.4shch.mongodb.net/?retryWrites=true&w=majority&appName=payments", dbPassword)
+	log.Printf("📝 URL de conexão com MongoDB: %s", uri)
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
 	if err != nil {
-
 		log.Fatal("Erro ao conectar ao MongoDB:", err)
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Ping(ctx, nil)
+	err = client.Ping(context.Background(), nil)
 	if err != nil {
-		log.Fatal("Erro ao conectar ao MongoDB:", err)
+		log.Fatal("Erro ao testar a conexão com o MongoDB:", err)
 	}
-
-	MongoDB = client.Database("users_db")
-	log.Println("Conectado ao MongoDB!")
+	log.Println("✅ Conexão com MongoDB realizada com sucesso!")
+	MongoDB = client.Database("paymentDB")
 }
--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"payments/config"
 	_ "payments/docs"
 	"payments/repository"
 	"payments/routes"
@@ -18,33 +14,11 @@ import (
 )
 
 func main() {
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Erro ao carregar o arquivo .env")
-	}
-
-	dbPassword := os.Getenv("DB_PASSWORD")
-	if dbPassword == "" {
-		log.Fatal("DB_PASSWORD não está definido")
-	}
-	uri := fmt.Sprintf("mongodb+srv://kaiquemotta:%s@payments.4shch.mongodb.net/?retryWrites=true&w=majority&appName=payments", dbPassword)
-	log.Printf("📝 URL de conexão com MongoDB: %s", uri)
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
-	if err != nil {
-		log.Fatal("Erro ao conectar ao MongoDB:", err)
-	}
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Fatal("Erro ao testar a conexão com o MongoDB:", err)
-	}
-	log.Println("✅ Conexão com MongoDB realizada com sucesso!")
-	db := client.Database("paymentDB")
-
-	repo := repository.NewPaymentRepository(db)
+	config.InitDB()
+	repo := repository.NewPaymentRepository(config.MongoDB)
 	useCase := usecase.NewPaymentUseCase(repo)
 
 	app := fiber.New()
-
 	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	log.Println("Registrando rotas de pagamento...")
@@ -54,7 +28,6 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-
 	log.Printf("📌 Servidor iniciando na porta: %s", port)
 
 	if err := app.Listen(":" + port); err != nil {
