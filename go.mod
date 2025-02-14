1f5b257dd8149b474a7a7e79ebac362d4c6d5aa4
Kaique Motta <kaique.motta@hotmail.com>
2025-02-13 23:14:11 -0300

    feat: sonar

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jarcoal/httpmock v1.3.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
--- test/usecase/payment_usecase_test.go
+++ test/usecase/payment_usecase_test.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"fmt"
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -164,5 +165,68 @@ func TestDeletePayment(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Equal(t, "payment not found: payment not found", err.Error())
 
+	mockRepo.AssertExpectations(t)
+}
+func TestProcessPaymentCallback(t *testing.T) {
+	// Mock do repositório de pagamentos
+	mockRepo := new(MockPaymentRepository)
+	// Criando uma instância do UseCase
+	useCase := usecase2.NewPaymentUseCase(mockRepo)
+
+	// Dados de callback simulados
+	callbackData := &domain.PaymentCallback{
+		PaymentID: "507f191e810c19729de860ea",
+		Status:    "completed",
+	}
+
+	// Criando o pagamento fictício
+	payment := domain.Payment{
+		ID:      primitive.NewObjectID(),
+		Amount:  150.0,
+		Status:  "pending",
+		OrderId: "order123",
+	}
+
+	// Mock para o repositório, retornando o pagamento simulado
+	mockRepo.On("GetByID", callbackData.PaymentID).Return(payment, nil)
+
+	// Agora simula a chamada HTTP para o serviço de pedidos
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Mock para a chamada HTTP POST para o microserviço de pedidos
+	httpmock.RegisterResponder("POST", "https://order.free.beeceptor.com",
+		httpmock.NewStringResponder(200, `{"status":"success"}`))
+
+	// Executando a função de callback
+	err := useCase.ProcessPaymentCallback(callbackData)
+
+	// Verificando se não houve erro
+	assert.Nil(t, err)
+
+	// Verificando se o mock do repositório foi chamado corretamente
+	mockRepo.AssertExpectations(t)
+}
+
+func TestProcessPaymentCallback_ErrorFetchingPayment(t *testing.T) {
+	// Mocking the payment repository
+	mockRepo := new(MockPaymentRepository)
+	useCase := usecase2.NewPaymentUseCase(mockRepo)
+
+	// Simulando os dados de callback de pagamento
+	callbackData := &domain.PaymentCallback{
+		PaymentID: "507f191e810c19729de860ea", // ID do pagamento
+		Status:    "completed",                // Status do pagamento
+	}
+
+	// Simulando erro ao buscar o pagamento
+	mockRepo.On("GetByID", callbackData.PaymentID).Return(domain.Payment{}, fmt.Errorf("payment not found"))
+
+	// Testando o processamento do callback do pagamento
+	err := useCase.ProcessPaymentCallback(callbackData)
+
+	// Verificações
+	assert.NotNil(t, err)                                                // Espera-se que ocorra um erro
+	assert.Equal(t, "payment not found: payment not found", err.Error()) // Verifica a mensagem de erro
 	mockRepo.AssertExpectations(t)
 }
